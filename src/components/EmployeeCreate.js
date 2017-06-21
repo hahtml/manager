fe974053382a5b2f0896882acb8ee664971dd73f
TML <tangmaoleismile@gmail.com>
2017-06-21 10:45:36 -0700

    add employee reducer

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { Button, Card, CardSection, Input } from './';
 
 class EmployeeCreate extends Component {
     render(){
         return (
-            <View>
-                <Text>
-                    employee create
-                </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Maolei"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        label="Phone Number"
+                        placeholder="222-222-2222"
+                    />
+                </CardSection>
+                <CardSection>
+
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         )
     }
 }
