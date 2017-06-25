c1ddf10d829473c6ebda5c3d014973fde37931ea
TML <tangmaoleismile@gmail.com>
2017-06-25 02:02:40 -0700

    add employee creation without action

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Button, Card, CardSection, Input } from './';
 import { Picker, Text } from 'react-native';
 
 class EmployeeCreate extends Component {
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        this.props.employeeCreate( { name, phone, shift: shift || 'Monday' } );
+    }
+
     render(){
         return (
             <Card>
@@ -40,7 +45,7 @@ class EmployeeCreate extends Component {
                     </Picker>
                 </CardSection>
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardSection>
@@ -65,4 +70,4 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps, { employeeUpdate })( EmployeeCreate );
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })( EmployeeCreate );
